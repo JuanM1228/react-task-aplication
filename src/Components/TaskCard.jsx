67a7ab08511afcf291eb0acb0012e24618aaf920
Juan Manuel Herrera Hernández <86576536+JuanM1228@users.noreply.github.com>
2023-03-01 23:43:52 -0600

    styles in card

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -3,7 +3,7 @@ import { MdDelete } from "react-icons/md";
 
 export const TaskCard = ({ task, key }) => {
   return (
-    <div>
+    <div className="taskCard">
       <h1>{task.title}</h1>
       <p>{task.description}</p>
       <MdDelete />
